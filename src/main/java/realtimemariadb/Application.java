d91ffff8f8aaa210b5c561606284717c8284ef7a
Naufal <achmadnaufalsyafiq@gmail.com>
2024-05-12 21:32:20 +0700

    refactor: use mysql-binlog-connector listener instead manually watch the binlogfile

--- src/main/java/realtimemariadb/Application.java
+++ src/main/java/realtimemariadb/Application.java
@@ -1,51 +1,77 @@
 package realtimemariadb;
 
-import com.github.shyiko.mysql.binlog.BinaryLogFileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.BinaryLogClient.EventListener;
 import com.github.shyiko.mysql.binlog.event.Event;
 import com.github.shyiko.mysql.binlog.event.EventType;
 import com.github.shyiko.mysql.binlog.event.QueryEventData;
 import com.github.shyiko.mysql.binlog.event.deserialization.EventDeserializer;
+
+import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
-import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.delete.Delete;
 import net.sf.jsqlparser.statement.insert.Insert;
-import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.update.Update;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class Application {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        
+        BinaryLogClient client = new BinaryLogClient("localhost", 3306, "admin", "test");
+        EventDeserializer eventDeserializer = new EventDeserializer();
+        eventDeserializer.setCompatibilityMode(
+                EventDeserializer.CompatibilityMode.DATE_AND_TIME_AS_LONG,
+                EventDeserializer.CompatibilityMode.CHAR_AND_BINARY_AS_BYTE_ARRAY);
+        client.setEventDeserializer(eventDeserializer);
+        client.registerEventListener(new EventListener() {
+            @Override
+            public void onEvent(Event event) {
+                if (event.getHeader().getEventType() == EventType.QUERY) {
+                    QueryEventData queryEventData = event.getData();
 
-        String binlogFile = "mybinlog-bin.000003";
-        String logDirectory = "C:\\Program Files\\MariaDB 11.0\\data";
+                    //parse the query from event
+                    Statement statement = null;
 
-        WatchService watchService = FileSystems.getDefault().newWatchService();
+                    try {
+                        statement = CCJSqlParserUtil.parse(queryEventData.getSql());
+                    } catch (JSQLParserException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
 
-        Path path = Paths.get(logDirectory);
+                    //get query type so we can cast statement to the appropriate class
+                    String queryType = statement.getClass().getSimpleName().toUpperCase();
+                    String tableName = null;
 
-        path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
-                StandardWatchEventKinds.ENTRY_DELETE,
-                StandardWatchEventKinds.ENTRY_MODIFY);
+                    switch (queryType) {
+                        case "INSERT":
+                            Insert insertStatement = (Insert) statement;
+                            tableName = insertStatement.getTable().getName();
+                            break;
+                        case "DELETE":
+                            Delete deleteStatement = (Delete) statement;
+                            tableName = deleteStatement.getTable().getName();
+                            break;
+                        case "UPDATE":
+                            Update updateStatement = (Update) statement;
+                            tableName = updateStatement.getTable().getName();
+                            break;
+                        default:
+                            System.out.println(String.format("unsupported query to watch %s", queryType));
+                    }
 
-        WatchKey watchKey = watchService.take();
-        while (true) {
-            List<WatchEvent<?>> pollEvents = watchKey.pollEvents();
-            for (WatchEvent event : pollEvents) {
-                if (binlogFile.toUpperCase().equals(event.context().toString().toUpperCase())) {
-                    List<Map> events = BinlogUtil.parseBinlogFile(logDirectory + "\\" + binlogFile);
-                    System.out.println(events);
-                    watchKey.reset();
+                    Map e = new HashMap();
+                    e.put("queryType", queryType);
+                    e.put("tableName", tableName);
+                    System.out.println(e.toString());
                 }
             }
-        }
+        });
+        client.connect();
     }
 }
